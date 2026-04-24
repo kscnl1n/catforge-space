a7cc14f72c97fb5bdcb6f9c6b0b9f8a6368dbe7a
Kaylee Scanlin <kscanlin03@gmail.com>
2026-04-24 03:06:07 -0500

    put on github

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
-
-
 /* mouse movement for bg */
-const bg = document.querySelector(".page-bg");
+const bg = document.querySelector('.page-bg, .officers-bg');
 
 document.addEventListener("mousemove", (e) => {
   const x = (e.clientX / window.innerWidth - 0.3);
@@ -12,6 +10,20 @@ document.addEventListener("mousemove", (e) => {
 
   bg.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.05)`;
 });
+
+/*
+const bg = document.querySelector(".officers-bg");
+
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.3);
+  const y = (e.clientY / window.innerHeight - 0.3);
+
+  const moveX = x * 30; // adjust strength
+  const moveY = y * 30;
+
+  bg.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.05)`;
+});
+*/
 
 const toggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
